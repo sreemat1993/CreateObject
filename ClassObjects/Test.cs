48a23ffb8eea95d071f09deb99eb44affcc98177
Sreemat <Sreemat@SREEMAT>
2017-01-21 12:51:15 +0530

    Add project files.

---
+++ ClassObjects/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestInfo
+{
+    public class Test
+    {
+        private string data;
+
+        public Test()
+        { }
+
+        public Test(string data)
+        {
+            this.data = data;
+        }
+
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.data;
+        }
+    }
+}
---
+++ CreateObject/ObjectCreator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TestInfo;
+using System.Runtime.Remoting;
+
+namespace CreateObject
+{
+    class ObjectCreator
+    {
+        public static void Main()
+        {
+            //First argument is Assembly Name(i.e dll name)
+            //Second argument is Namespace and class name
+            ObjectHandle handle = Activator.CreateInstance("Sample", "TestInfo.Test");
+            var createdObject = handle.Unwrap();
+            var typeCastedCreatedObject=Convert.ChangeType(createdObject, createdObject.GetType());
+            var prop = typeCastedCreatedObject.GetType().GetProperty("Data");
+            if (prop != null)
+                prop.SetValue(typeCastedCreatedObject, "Sreemat");
+            var methodInfo = typeCastedCreatedObject.GetType().GetMethod("get_Data");
+            var methodResult = methodInfo.Invoke(typeCastedCreatedObject, null);
+            if (methodResult != null)
+                Console.WriteLine(methodResult);
+            Console.ReadLine();
+        }
+    }
+}
